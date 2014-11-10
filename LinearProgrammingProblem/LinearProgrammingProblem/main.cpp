c8f96a28ce0e5feb25c86e62cc847a892dc672c9
alumi <alumi@tigerparty.com>
2014-11-10 23:10:40 +0800

    Add "using namespace std"

--- LinearProgrammingProblem/LinearProgrammingProblem/main.cpp
+++ LinearProgrammingProblem/LinearProgrammingProblem/main.cpp
@@ -8,8 +8,10 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
     std::cout << "Hello, World!\n";
     return 0;
 }
